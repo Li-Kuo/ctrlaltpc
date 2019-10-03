a5ac59170338c53eeb7a76bfb645932cc0054e59
Li-Kuo <200414666@student.georgianc.on.ca>
2019-10-03 12:31:49 -0400

    test

--- CtlAltPc/Models/Product.cs
+++ CtlAltPc/Models/Product.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -41,3 +42,48 @@ namespace CtlAltPc.Models
         public virtual ICollection<OrderDetail> OrderDetail { get; set; }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CtlAltPc.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Cart = new HashSet<Cart>();
+            OrderDetail = new HashSet<OrderDetail>();
+        }
+
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is Required")]
+        [StringLength(255)]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [StringLength(8000)]
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(10, 2)")]
+        [Range(0, 999999, ErrorMessage = "Price must be between 0 and 999,999")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Price { get; set; }
+
+        [StringLength(255)]
+        public string Photo { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        [InverseProperty("Product")]
+        public virtual Category Category { get; set; }
+        [InverseProperty("Product")]
+        public virtual ICollection<Cart> Cart { get; set; }
+        [InverseProperty("Product")]
+        public virtual ICollection<OrderDetail> OrderDetail { get; set; }
+    }
+}
+>>>>>>> project1/master
