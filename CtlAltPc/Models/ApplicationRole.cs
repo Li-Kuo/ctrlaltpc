10c51d7f51cc4ecbeda41549cee77935f98cb447
Li-Kuo <55146208+Li-Kuo@users.noreply.github.com>
2019-10-23 20:06:59 -0400

    Add files via upload

---
+++ CtlAltPc/Models/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CtlAltPc.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+    }
+}
